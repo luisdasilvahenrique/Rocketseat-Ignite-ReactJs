fe9755ceacf23827b4c513c23c528ed8b4e5157c
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-02-21 23:03:53 -0300

    webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,10 @@ module.exports = {
     resolve: {
         extensions: ['.js', '.jsx'],
     },
+    devServer: {
+        static: path.resolve(__dirname, 'public'),
+         
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'public', 'index.html')
